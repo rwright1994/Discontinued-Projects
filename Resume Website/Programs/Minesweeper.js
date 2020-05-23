714da76c79680c07b19c92daf8807774d8ae5149
rwright1994 <robertstephenwright94@gmail.com>
2020-05-23 19:01:55 -0300

    Fixed issue with last div in tree overlapping with footer by adding a padding to the body and making the foot a fixed height. Added media quary for reponsive design. Created contact page.

--- Resume Website/Programs/Minesweeper.js
+++ Resume Website/Programs/Minesweeper.js
@@ -1,14 +1,12 @@
-var gameArea ={
-    canvas : document.createElement("canvas"),
-    start: fuction(){
-      this.canvas.width = 800;
-      this.canvas.height = 800;
-      this.context = this.canvas.getContext("2d")
-      document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    }
-}
+window.onload = function() {
+    var canvas = document.getElementById("gameBoard");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, 0, 150, 75);
+    moveCanvas();
+};
 
-
-function startGame(){
-  gameArea.start();
+function moveCanvas(){
+    document.getElementById("gameBoard").style.backgroundColor = "blue";
+    document.getElementById("gameBoard").style.left = "-100px";
 }
