1472c902bce5c403c813de01767a53c9dc29d2d0
rwright1994 <robertstephenwright94@gmail.com>
2020-05-26 01:19:25 -0300

    Added selectedable tabs for each program that is registered under the programs nav link. Created a canvas that handles an onclick that print's out to console. Moved <body> to reflect correct document flow.

--- Resume Website/Programs/Minesweeper.js
+++ Resume Website/Programs/Minesweeper.js
@@ -1,12 +1,25 @@
-window.onload = function() {
-    var canvas = document.getElementById("gameBoard");
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0, 0, 150, 75);
-    moveCanvas();
-};
 
-function moveCanvas(){
-    document.getElementById("gameBoard").style.backgroundColor = "blue";
-    document.getElementById("gameBoard").style.left = "-100px";
+function startGame(){
+  var canvas = document.getElementById("gameBoard");
+  document.getElementById("gameBoard").addEventListener('click', reveal);
+
+  canvas.width = 600;
+  canvas.height= 600;
+  var ctx = canvas.getContext("2d");
+  var blockWidth = (canvas.width - 2) / 8;
+  var blockHeight = (canvas.height - 2 )/ 8;
+
+  var i;
+  var j;
+  for( i = 0; i < 8; i++){
+    for(j=0; j < 8; j++){
+      ctx.fillRect(blockWidth * i +1, blockHeight * j +1, 74,74);
+      ctx.stroke();
+
+    }
+  }
+}
+
+function reveal(){
+  console.log("Clicked canvas.");
 }
