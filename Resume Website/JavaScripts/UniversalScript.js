c4ed384293444735c730ba0be2b9b24396a926a1
rwright1994 <robertstephenwright94@gmail.com>
2020-06-01 19:09:26 -0300

    Created new UniversalScript.js file to apply JS to certain universal elements. Also added a removeProperty to restore proper document flow when screen size changes.

---
+++ Resume Website/JavaScripts/UniversalScript.js
@@ -0,0 +1,9 @@
+function openNav(){
+	document.getElementById("navbar").style.width = "180px";
+
+}
+
+function closeNav(){
+	document.getElementById("navbar").style.width = "0";
+	document.getElementById("navbar").style.removeProperty('width');
+}
